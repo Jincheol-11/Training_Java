7179b99a85fed538307213f729123a301e634f75
Jincheol <sunkiro96@naver.com>
2023-10-18 00:45:40 +0900

    나머지 연산자를 활용한 연산

---
+++ RemainderOperation.java
@@ -0,0 +1,8 @@
+package TrainingJava;
+
+public class Training2 {
+    public static void main(String[] args) {
+        int num = 24;
+        System.out.println(10 - num%10); //나머지 연산자를 통해 num%10 = 4를 얻음 
+    }
+}
