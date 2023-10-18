b46921130924451a69be719184883210161d3d73
Jincheol-11 <145963704+Jincheol-11@users.noreply.github.com>
2023-10-18 09:18:38 +0900

    Update RemainderOperation.java

--- RemainderOperation.java
+++ RemainderOperation.java
@@ -1,6 +1,6 @@
 package TrainingJava;
 
-public class Training2 {
+public class RemainderOperation {
     public static void main(String[] args) {
         int num = 24;
         System.out.println(10 - num%10); //나머지 연산자를 통해 num%10 = 4를 얻음 
