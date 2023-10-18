91947b748fec7e98a41927838abcef9f9a7234ab
Jincheol-11 <145963704+Jincheol-11@users.noreply.github.com>
2023-10-18 19:54:04 +0900

    Delete RemainderOperate.java

--- RemainderOperate.java
+++
@@ -1,8 +0,0 @@
-package com.mystudy.run.TrainingJava;
-
-public class RemainderOperation {
-    public static void main(String[] args) {
-        int num = 24;
-        System.out.println(10 - num%10); //나머지 연산자를 통해 num%10 = 4를 얻음
-    }
-}
