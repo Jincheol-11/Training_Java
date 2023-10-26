e42c79371cc0b746c1af37d0ae25da507212656a
jincheol <sunkiro96@naver.com>
2023-10-26 19:45:43 +0900

    진수 변환하기

---
+++ Essential of Java/ConvertDecimalNumber.java
@@ -0,0 +1,14 @@
+package com.mystudy.run.TrainingJava;
+
+public class ConvertDecimalNumber {
+    public static void main(String[] args) {
+            int var1 = 0b101101; // 앞 2자리가 0b 이기에
+            int var2 = 0461702; // 숫자만으로 이루어져 있으므로 10진수
+            int var3 = 0xB1266FA; // 0x 는 16진수
+
+            System.out.println("var1:" + var1);
+            System.out.println("var2:" + var2);
+            System.out.println("var3:" + var3);
+        }
+    }
+
